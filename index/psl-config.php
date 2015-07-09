e4b913f948b841781f59c6b0477978f63cb541e2
Nippy <MayorOfCrabtown@gmail.com>
2015-07-09 20:02:26 +1000

    Update psl-config.php

--- index/psl-config.php
+++ index/psl-config.php
@@ -10,4 +10,3 @@ define("CAN_REGISTER", "any");
 define("DEFAULT_ROLE", "secure_login");
  
 define("SECURE", TRUE);    // Should be set to TRUE in a production environment, only use FALSE for development/testing
-?>
