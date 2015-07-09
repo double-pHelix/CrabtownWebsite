4c81e1b0f14a43095dd21bf811481b413d9b07b5
Nippy <MayorOfCrabtown@gmail.com>
2015-07-09 20:34:49 +1000

    Update psl-config.php

--- index/psl-config.php
+++ index/psl-config.php
@@ -2,8 +2,8 @@
 /*Global configuration variables*/
 
 define("HOST", "localhost");     // The host you want to connect to.
-define("USER", "unreg");    // The database username. 
-define("PASSWORD", "notreg");    // The database password. 
+define("USER", "siteuser");    // The database username. 
+define("PASSWORD", "notadmin");    // The database password. 
 define("DATABASE", "users");    // The database name.
  
 define("CAN_REGISTER", "any");
