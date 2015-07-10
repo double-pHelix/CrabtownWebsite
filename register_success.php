085eb0a26dd6413334e00b89dd07646db013ac49
Nippy <MayorOfCrabtown@gmail.com>
2015-07-10 21:31:41 +1000

    Create register_success.php

---
+++ register_success.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Secure Login: Registration Success</title>
+        <link rel="stylesheet" href="styles/main.css" />
+    </head>
+    <body>
+        <h1>Welcome to Crabtown!</h1>
+        <p><a href="index.php">Log in</a> and begin exploring Crabtown!</p>
+    </body>
+</html>
