cfc857e595e0ac82e36b9fa2db7ea05d6432aeb0
Nippy <MayorOfCrabtown@gmail.com>
2015-07-11 14:57:11 +1000

    Update register.php

--- register.php
+++ register.php
@@ -11,7 +11,7 @@ include_once 'includes/emailcheck.php';
         <title>Secure Login: Registration Form</title>
         <script type="text/JavaScript" src="js/sha512.js"></script> 
         <script type="text/JavaScript" src="js/forms.js"></script>
-        <link rel="stylesheet" href="styles/main.css" />
+        <link rel="stylesheet" href="css/register.css" />
     </head>
     <body>
         <!-- Registration form to be output if the POST variables are not
