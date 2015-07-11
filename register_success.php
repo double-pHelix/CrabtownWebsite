5e1d7ad50dd471e56ff082edd4c6da368aff0793
Nippy <MayorOfCrabtown@gmail.com>
2015-07-11 19:10:37 +1000

    Update register_success.php

--- register_success.php
+++ register_success.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <title>Secure Login: Registration Success</title>
-        <link rel="stylesheet" href="styles/main.css" />
+        <link rel="stylesheet" href="styles/register_success.css" />
     </head>
     <body>
         <h1>Welcome, citizen!</h1>
