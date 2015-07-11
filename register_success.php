f78e81904d6e9c334d39280fa435520ce03877b7
Nippy <MayorOfCrabtown@gmail.com>
2015-07-11 19:17:19 +1000

    Update register_success.php

--- register_success.php
+++ register_success.php
@@ -9,7 +9,7 @@
         <h1>Welcome, citizen!</h1>
         <img src="registration_success.png">
         <?php 
-            echo "<div id="citizen_name">".$_SESSION['username']."</div>";
+            echo "<div id="citizen_name">".$_SESSION['username']."</div>";?>
         <p><a href="index.php">Log in</a> and begin exploring Crabtown!</p>
     </body>
 </html>
