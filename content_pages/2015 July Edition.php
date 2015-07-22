60f4d50917a42f8088728ba55b8eb1011c07d894
double-pHelix <led_python@hotmail.com>
2015-07-22 16:58:32 +1000

    Testing login pop up with another page.

--- content_pages/2015 July Edition.php
+++ content_pages/2015 July Edition.php
@@ -12,27 +12,44 @@
 <!--[if IE 9 ]>    <html lang="en" class="ie9"> <![endif]-->
 <!--[if !IE]><!--> <html lang="en"> <!--<![endif]-->
 <head>
+   <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        
+    <script type="text/JavaScript" src="/js/sha512.js"></script> 
+    <script type="text/JavaScript" src="/js/forms.js"></script>
+    
+    <link rel="stylesheet" type="text/css" href="/css/Crabtown v1.0.css">
+    <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/login.css"/>
+    
+    <link href='http://fonts.googleapis.com/css?family=Londrina+Solid' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Londrina+Shadow' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,400italic,600' rel='stylesheet' type='text/css'>   
+    
+    <link rel="stylesheet" href="/css/login_menu.css"/>
+    
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+   
+    <script type="text/javascript" 
+    src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script> 
+    <script type="text/JavaScript" src="/js/popup.js"></script>
+
+
   <meta name="viewport" content="width = 1050, user-scalable = no" />
   <script type="text/javascript" src="/CSS/extras/jquery.min.1.7.js"></script>
   <script type="text/javascript" src="/CSS/extras/modernizr.2.5.3.min.js"></script>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <link href='http://fonts.googleapis.com/css?family=Londrina+Solid' rel='stylesheet' type='text/css'>
-  <link href='http://fonts.googleapis.com/css?family=Londrina+Shadow' rel='stylesheet' type='text/css'>
-  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,400italic,600' rel='stylesheet' type='text/css'>
-    
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-  <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-      
-  <link rel="stylesheet" type="text/css" href="/css/Crabtown v1.0.css">
+          
   <link href="carousel.css" rel="stylesheet">
 
 </head>
 <body id="Crablar_Reader">
     <!-- Navigation Menu at the top of each page -->
     <?php include_once $_SERVER['DOCUMENT_ROOT'].'/menu_navigation.php'; ?>
- 
+    
+    <!-- Testing pop up menu -->
+    <a href="#?w=500" rel="03" class="poplight">LINKS</a>
+      
 <?php if (login_check($mysqli) == true) : ?>
    
   <div class="flipbook-viewport">
@@ -94,4 +111,12 @@
   </p>
 <?php endif; ?>
 </body>
+
+<!-- Menu that pops up -->
+<div id="03" class="popup_block">
+  <Center>
+    <?php include_once $_SERVER['DOCUMENT_ROOT'].'/login_menu.php'; ?>
+  </center>
+</div>
+
 </html>
