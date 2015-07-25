45ea01300b7586e2aa22359d46ddfa1d488f8c56
double-pHelix <led_python@hotmail.com>
2015-07-26 00:08:41 +1000

    Update schema with users in correct groups.

--- database_stuff/crabtowndbschema.sql
+++ database_stuff/crabtowndbschema.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 25, 2015 at 10:03 AM
+-- Generation Time: Jul 25, 2015 at 10:08 AM
 -- Server version: 5.5.44-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.11
 
@@ -163,9 +163,8 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `email`, `group_id`, `password`, `salt`, `perm_override_remove`, `perm_override_add`, `reg_date`, `last_login_date`, `reg_ip`, `last_login_ip`, `must_validate`) VALUES
-(4, 'Greg', 'felixphu@hotmail.com', 0, '540b4dd71c30112cf9a83f395f8f48da9d473bf2d98de24926d11a6c98e249eb7dda6989b5e8eef14b9a1cd3e2c03b238ef9843fd7bc0b0b624006f9a992fa8d', 'bea4b9cdb5eb402869b32c004c1e8ebf0e5a95ed390fefa5d5fceefe426dc1783dc6901d9d1461f15c51f90ea9734314288852a82e1076d5633879130c0f2fbf', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0, 0),
-(5, 'Nippy', 'MayorOfCrabtown@gmail.com', 0, '898a7c165088a047fea205ecd949e59a0e5df26d47bde6a5c89ccbcc485f41fbb04d8dbd38d6de84de471c9b2016a9643e75856068b768390f9731f6e5b9795d', 'dcdef9dae4586bf5b918dc99d7307a09f2ce612711a4d386ea634ce13ff6f635dc189942f71423ee165e5578d9880f8b0f9e88c857855f7465b7c1438d0273ea', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0, 0),
-(6, 'Greg3', 'adley.phu@gmail.com', 0, '079d08f1799d7e11e71fa88d09fc7c5c5c25f3ae28ab1b3c318b26358c6b30ea1e8b7be511f046269807fc6fe441198463e147bed73acc25b0a2f23cb0708876', 'efc191005c65bec3eb6e9fad7fd5957e1c6d92cee050f63130a819861b394f5757cc61c65e1a886e86f6b6f9ad894160dbc73483e64223ccdff0fc6128d41089', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0, 0);
+(4, 'Greg', 'notreal@hotmail.com', 1, '540b4dd71c30112cf9a83f395f8f48da9d473bf2d98de24926d11a6c98e249eb7dda6989b5e8eef14b9a1cd3e2c03b238ef9843fd7bc0b0b624006f9a992fa8d', 'bea4b9cdb5eb402869b32c004c1e8ebf0e5a95ed390fefa5d5fceefe426dc1783dc6901d9d1461f15c51f90ea9734314288852a82e1076d5633879130c0f2fbf', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0, 0),
+(5, 'Nippy', 'MayorOfCrabtown@gmail.com', 1, '898a7c165088a047fea205ecd949e59a0e5df26d47bde6a5c89ccbcc485f41fbb04d8dbd38d6de84de471c9b2016a9643e75856068b768390f9731f6e5b9795d', 'dcdef9dae4586bf5b918dc99d7307a09f2ce612711a4d386ea634ce13ff6f635dc189942f71423ee165e5578d9880f8b0f9e88c857855f7465b7c1438d0273ea', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0, 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
