806614185852b70dc7675cbf7e6d09ee5c8e6c6c
Nippy <MayorOfCrabtown@gmail.com>
2015-07-25 15:47:43 +1000

    Update crablar_archives_flipbook.php

--- content_pages/crablar_archives_flipbook.php
+++ content_pages/crablar_archives_flipbook.php
@@ -65,8 +65,18 @@ include_once $_SERVER['DOCUMENT_ROOT'].'/includes/functions.php';
 	  <option value="2014">2014</option>
 	</select> 
 	<select>
-	  <option value=""></option>
-	  <option value=""></option>
+	  <option value="Jan">Jan</option>
+	  <option value="Feb">Feb</option>
+	  <option value="Mar">Mar</option>
+	  <option value="Apr">Apr</option>
+	  <option value="May">May</option>
+	  <option value="Jun">Jun</option>
+	  <option value="July">July</option>
+	  <option value="Aug">Aug</option>
+	  <option value="Sep">Sep</option>
+	  <option value="Oct">Oct</option>
+	  <option value="Nov">Nov</option>
+	  <option value="Dec">Dec</option>
 	</select>
 </div>	
 	
