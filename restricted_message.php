5053319b6837d5257174c53889e519645bcdfd9e
double-pHelix <led_python@hotmail.com>
2015-07-25 21:25:35 +1000

    Update restricted_message.php

--- restricted_message.php
+++ restricted_message.php
@@ -1,4 +1,4 @@
 <p>
-    <span class="error">Only citizens of Crabtown are permitted access to these top secret files.</span> Please <a href="#?w=500" rel="03" class="poplight">login</a>.
+    <span class="error">Only citizens of Crabtown are permitted access to these top secret files.</span> Please <a data-toggle="modal" data-target="#myModal">login</a>.
 </p>
 <img src="/images/restricted.png">
