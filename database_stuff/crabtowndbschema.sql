714de1f3843c1a515b85de43472e3fdff9980057
double-pHelix <led_python@hotmail.com>
2015-07-26 00:03:50 +1000

    (actual new schema)

--- database_stuff/crabtowndbschema.sql
+++ database_stuff/crabtowndbschema.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 25, 2015 at 09:59 AM
+-- Generation Time: Jul 25, 2015 at 10:03 AM
 -- Server version: 5.5.44-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.11
 
@@ -19,6 +19,119 @@ SET time_zone = "+00:00";
 --
 -- Database: `crabtown_database`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `autologin`
+--
+
+CREATE TABLE IF NOT EXISTS `autologin` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `public_key` varchar(32) NOT NULL,
+  `private_key` varchar(32) NOT NULL,
+  `created_on` datetime NOT NULL,
+  `last_used_on` datetime NOT NULL,
+  `last_used_ip` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `public_key` (`public_key`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `banned_emails`
+--
+
+CREATE TABLE IF NOT EXISTS `banned_emails` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `email_regex` varchar(120) NOT NULL,
+  `created_on` datetime NOT NULL,
+  `notes` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `crablars`
+--
+
+CREATE TABLE IF NOT EXISTS `crablars` (
+  `year` int(4) NOT NULL,
+  `month` varchar(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `forced_group_ips`
+--
+
+CREATE TABLE IF NOT EXISTS `forced_group_ips` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `group_id` bigint(20) NOT NULL,
+  `ip_low` int(10) unsigned NOT NULL,
+  `ip_high` int(10) unsigned NOT NULL,
+  `created_on` datetime NOT NULL,
+  `notes` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ip_low` (`ip_low`),
+  KEY `ip_high` (`ip_high`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `groups`
+--
+
+CREATE TABLE IF NOT EXISTS `groups` (
+  `id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `group_name` varchar(32) NOT NULL,
+  `permissions` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `groups`
+--
+
+INSERT INTO `groups` (`id`, `group_name`, `permissions`) VALUES
+(1, 'standard', 63);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login_attempts`
+--
+
+CREATE TABLE IF NOT EXISTS `login_attempts` (
+  `user_id` int(11) NOT NULL,
+  `time` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login_attempts`
+--
+
+INSERT INTO `login_attempts` (`user_id`, `time`) VALUES
+(3, 1437235963),
+(4, 1437624670),
+(4, 1437624685),
+(4, 1437624960),
+(4, 1437625613),
+(4, 1437625709),
+(4, 1437626012),
+(4, 1437635207),
+(4, 1437712778),
+(4, 1437712784),
+(4, 1437751438),
+(4, 1437807825),
+(4, 1437823601);
 
 -- --------------------------------------------------------
 
