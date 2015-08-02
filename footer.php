7d883b50df7d802bc57cd5e8a83077a99542641d
double-pHelix <led_python@hotmail.com>
2015-08-02 23:45:33 +1000

    Fixed wave footer image. it looked weird before.

--- footer.php
+++ footer.php
@@ -1,4 +1,6 @@
 <footer id="footer">
-  <p class="pull-right"><a href="#">Back to top</a></p>
-  <p>&copy; 2015 Crabtown, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+  <div id="footer_text">
+    <p class="pull-right"><a href="#">Back to top</a></p>
+    <p>&copy; 2015 Crabtown, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+  </div>
 </footer>
