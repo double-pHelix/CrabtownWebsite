8b748dc2ab8a0cb8acd41f6c0cd080472598352e
Nippy <MayorOfCrabtown@gmail.com>
2015-08-02 12:44:57 +1000

    Update crablar_archives_flipbook.php

--- content_pages/crablar_archives_flipbook.php
+++ content_pages/crablar_archives_flipbook.php
@@ -104,8 +104,7 @@ if (isset($_POST['username'], $_POST['p']) && $_POST['form_type'] == "login") {
 			}
 			
 			function latest_crablar (){
-				$latest = "SELECT MAX(edition_no) FROM crablar_archives";
-				$result = $mysqli->query($latest);
+				$result = $mysqli->query("SELECT MAX(edition_no) FROM crablar_archives");
 				
 				if ($row = mysql_fetch_assoc($result)) {
 				   $year = $row['year'];
@@ -131,7 +130,7 @@ if (isset($_POST['username'], $_POST['p']) && $_POST['form_type'] == "login") {
 				$year = $_POST['year'];
 				$month = $_POST['month'];
 				$stmt->execute();
-				$row = mysql_fetch_assoc($stmt);
+				$row = mysqli_fetch_assoc($stmt);
 				
 				//checks if query returns result and how many
 				//should be 1 result max, thinking about implementing search by year option
