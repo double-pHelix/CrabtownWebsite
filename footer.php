f19cee74b858294704edca69a001409ed5487791
Nippy <MayorOfCrabtown@gmail.com>
2015-08-02 17:48:04 +1000

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <footer>
   <p class="pull-right"><a href="#">Back to top</a></p>
   <p>&copy; 2015 Crabtown, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+  <img src="/images/wave.png" id="footerimg">
 </footer>
