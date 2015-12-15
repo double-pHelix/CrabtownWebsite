940aadc4b9e30303a450c2170b1c471720f0d765
double-pHelix <led_python@hotmail.com>
2015-12-15 18:58:47 +1100

    Removed Sensitive Information (They have been changed anyway)

--- includes/psl-config.php
+++ includes/psl-config.php
@@ -2,11 +2,13 @@
 /*Global configuration variables*/
 
 define("HOST", "localhost");     // The host you want to connect to.
-define("USER", "nippy");    // The database username. 
-define("PASSWORD", "howerpower");    // The database password. 
+define("USER", "");    // The database username. 
+define("PASSWORD", "");    // The database password. 
 define("DATABASE", "crabtown_database");    // The database name.
  
 define("CAN_REGISTER", "any");
 define("DEFAULT_ROLE", "secure_login");
  
 define("SECURE", TRUE);    // Should be set to TRUE in a production environment, only use FALSE for development/testing
+define("CONSUMER_KEY", "");
+define("CONSUMER_SECRET", "");
